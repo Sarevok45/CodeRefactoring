a683391e8f6d752ebbce5f56b7c3a8d99def4d2a
student <student@lab-43-7.cs.put.poznan.pl>
2018-05-25 12:46:13 +0200

    Initial commit

---
+++ src/com/bartoszwalter/students/taxes/config/ContractType.java
@@ -0,0 +1,5 @@
+package com.bartoszwalter.students.taxes.config;
+
+public enum ContractType {
+    UMOWA_ZLECENIE, UMOWA_O_PRACE;
+}
---
+++ src/com/bartoszwalter/students/taxes/config/WorkParms.java
@@ -0,0 +1,16 @@
+package com.bartoszwalter.students.taxes.config;
+
+import java.math.BigDecimal;
+
+public class WorkParms {
+    private BigDecimal Podstawa;
+    private ContractType TypUmowy;
+
+    private double ProcentSkladkaEmerytura = 9.76;
+    private double ProcentSkladkaRenta = 1.5;
+    private double ProcentSkladkaZdrowotne = 2.45;
+
+
+
+
+}
