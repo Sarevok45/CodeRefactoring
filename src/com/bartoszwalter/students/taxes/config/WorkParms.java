0b4861d5868fbc4649c6a27ffad78e70c4cc6863
sarevok45 <$DanaScully45>
2018-05-25 13:00:41 +0200

    First version of WorkParams

--- src/com/bartoszwalter/students/taxes/config/WorkParms.java
+++ src/com/bartoszwalter/students/taxes/config/WorkParms.java
@@ -10,7 +10,84 @@ public class WorkParms {
     private double ProcentSkladkaRenta = 1.5;
     private double ProcentSkladkaZdrowotne = 2.45;
 
+    public WorkParms() {
+        Podstawa = new BigDecimal(0);
+        TypUmowy = ContractType.UMOWA_O_PRACE;
+    }
+
+    public WorkParms(String podstawa, ContractType typUmowy) {
+        Podstawa = new BigDecimal(podstawa);
+        TypUmowy = typUmowy;
+    }
+
+    public WorkParms(double podstawa, ContractType typUmowy) {
+        Podstawa = new BigDecimal(podstawa);
+        TypUmowy = typUmowy;
+    }
 
 
+    public WorkParms(BigDecimal podstawa, ContractType typUmowy) {
+        Podstawa = podstawa;
+        TypUmowy = typUmowy;
+    }
 
+
+    public WorkParms(BigDecimal podstawa, ContractType typUmowy, double procentSkladkaEmerytura, double procentSkladkaRenta, double procentSkladkaZdrowotne) {
+        Podstawa = podstawa;
+        TypUmowy = typUmowy;
+        ProcentSkladkaEmerytura = procentSkladkaEmerytura;
+        ProcentSkladkaRenta = procentSkladkaRenta;
+        ProcentSkladkaZdrowotne = procentSkladkaZdrowotne;
+    }
+
+
+    public BigDecimal getPodstawa() {
+        return Podstawa;
+    }
+
+
+    public void setPodstawa(BigDecimal podstawa) {
+        Podstawa = podstawa;
+    }
+
+    public void setPodstawa(String podstawa) {
+        Podstawa = new BigDecimal(podstawa);
+    }
+
+    public void setPodstawa(double podstawa) {
+        Podstawa = new BigDecimal(podstawa);
+    }
+    
+
+    public ContractType getTypUmowy() {
+        return TypUmowy;
+    }
+
+    public void setTypUmowy(ContractType typUmowy) {
+        TypUmowy = typUmowy;
+    }
+
+    public double getProcentSkladkaEmerytura() {
+        return ProcentSkladkaEmerytura;
+    }
+
+    public void setProcentSkladkaEmerytura(double procentSkladkaEmerytura) {
+        ProcentSkladkaEmerytura = procentSkladkaEmerytura;
+    }
+
+    public double getProcentSkladkaRenta() {
+        return ProcentSkladkaRenta;
+    }
+
+    public void setProcentSkladkaRenta(double procentSkladkaRenta) {
+        ProcentSkladkaRenta = procentSkladkaRenta;
+    }
+
+    public double getProcentSkladkaZdrowotne() {
+        return ProcentSkladkaZdrowotne;
+    }
+
+    public void setProcentSkladkaZdrowotne(double procentSkladkaZdrowotne) {
+        ProcentSkladkaZdrowotne = procentSkladkaZdrowotne;
+    }
 }
